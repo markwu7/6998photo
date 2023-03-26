d7b94a2abfddc89bd80cfc5085fd205fc7691d7b
Thomas Zhang <jz3516@columbia.edu>
2023-03-25 22:19:41 -0400

    completed search functionality

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
+// import { APIGatewayClient, CreateApiKeyCommand } from "@aws-sdk/client-api-gateway";
+// var apigClientFactory = require('aws-api-gateway-client').default;
+// import apigClientFactory from APIGatewayClient;
+
 function search() {
-    var searchTerm = document.getElementById("searchbar").value;
-    var apigClient = apigClientFactory.newClient({ apiKey: “xxxxxxxxxxxx” });
-  
-    var params = {
-      "q": searchTerm
-    };
-  
-    apigClient.searchGet(params, {}, {})
-      .then(function (result) {
-        showImages(result.data);
-      }).catch(function (result) {
-        console.log(result);
-      });
+  var searchTerm = document.getElementById("searchbar").value;
+  console.log(searchTerm)
+  var apigClient = apigClientFactory.newClient({ apiKey: "bDIGszy43q1tj2xYi0WTZ5lEq6s5SdbY7DhKDAC0" });
+
+  var params = {
+    "q": searchTerm
+  };
+
+  apigClient.searchGet(params, {}, {headers: {"Access-Control-Allow-Origin": "*"}})
+    .then(function (result) {
+      showImages(result.data);
+    }).catch(function (result) {
+      console.log(result);
+    });
+}
+
+function showImages(data) {
+  var imagesDiv = document.getElementById("images")
+  var urls = data["results"]
+
+  var complete = ""
+  var row = "<div class=\"row\">"
+  for (let i = 0; i < urls.length; i++) {
+    row += `<img src=${urls[i]} height="500">`
+    if (i % 5 == 0) {
+      row += "</div>"
+      complete += row
+      row = "<div class=\"row\">"
+    }
   }
 
-
-function showImages() {
-
-
+  if (row != "<div class=\"row\">") {
+    complete += row
+    complete += "</div>"
+  }
+  console.log(complete)
+  imagesDiv.innerHTML += complete
 }
