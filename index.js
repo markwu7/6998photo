b43c070ac226de34c2eee9c543b2855a43bc34a9
“Mark <hwu378@wisc.edu>
2023-03-27 17:49:27 -0400

    upload function

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+var apigClient = apigClientFactory.newClient({ apiKey: "bDIGszy43q1tj2xYi0WTZ5lEq6s5SdbY7DhKDAC0" });
 function search() {
   var searchTerm = document.getElementById("searchbar").value;
   console.log(searchTerm)
-  var apigClient = apigClientFactory.newClient({ apiKey: "bDIGszy43q1tj2xYi0WTZ5lEq6s5SdbY7DhKDAC0" });
+  
 
   var params = {
     "q": searchTerm
@@ -39,27 +40,49 @@ function showImages(data) {
 }
 
 function upload() {
-  let image = document.getElementById("image").value;
+  let image_src = document.getElementById("image");
+  let image = image_src.value;
 	let labels = document.getElementById("labels").value;
   let imageType = image.slice(image.lastIndexOf(".") + 1);
   let imageNameWithExtension = image.split('\\').pop();
   let imageName = imageNameWithExtension.slice(0, imageNameWithExtension.lastIndexOf('.'));
-  console.log(imageName)
+  // console.log(imageName)
   // console.log(image.split('\\').pop())
   // console.log(image.slice(image.lastIndexOf(".") + 1))
 
-  let config = {
-    headers:{
-      "Content-Type": imageType, 
-      "x-api-key": "bDIGszy43q1tj2xYi0WTZ5lEq6s5SdbY7DhKDAC0", 
-      "x-amz-meta-customLabels": labels
-    }
-  };
-  console.log(config)
+  const file = image_src.files[0];
+  const reader = new FileReader();
 
-  // url = 'https://e1371dfvrl.execute-api.us-east-1.amazonaws.com/coms6998-photos-bucket' + imageName
-  // axios.put(url,imageNameWithExtension,config).then(response=>{
-  //   window.alert("Successfully Upload!");
-  // })
+  reader.addEventListener('load', function() {
+      var body = reader.result;
+      
+      
+      // Do something with the binary data
+      console.log(binary)
+      let config = {
+        headers:{
+          "Content-Type": "application/json", 
+          "x-api-key": "bDIGszy43q1tj2xYi0WTZ5lEq6s5SdbY7DhKDAC0", 
+          "x-amz-meta-customLabels": labels,
+          "Access-Control-Allow-Origin": "*"
+        }
+      };
+      apigClient.invokeApi({}, {}, "PUT", config, body)
+      .then(function(result){
+          //This is where you would put a success callback
+          console.log(result);
+      }).catch( function(result){
+          //This is where you would put an error callback
+      });
+      
+    
+      // url = 'https://e1371dfvrl.execute-api.us-east-1.amazonaws.com/test-stage/upload/coms6998-photos-bucket/' + imageNameWithExtension
+      // axios.put(url,binary,config).then(response=>{
+      //   window.alert("Successfully Upload!");
+      // })
+  });
+  
 
+  
+  // reader.readAsBinaryString(file);
 }
